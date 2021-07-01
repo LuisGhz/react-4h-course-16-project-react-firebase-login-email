45564a0626d871dac3b5d2233dda0cecd3528e96
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-01 14:24:44 -0500

    Set signin class to container

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -2,7 +2,7 @@ import './Signin.css';
 
 const Signin = () => {
   return (
-  <div>
+  <div className="signin">
     <form action="">
       <h1>Sign in</h1>
       <input type="text" name="" id="" />
