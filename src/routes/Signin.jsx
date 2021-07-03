224672c5dbe6e8e76f8022906ecf07e331d50553
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-03 13:30:55 -0500

    Create singIn function

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -15,13 +15,23 @@ const Signin = () => {
     .then( user => console.log(user) )
     .catch( err => console.error(err) );
   }
+
+  const singIn = e => {
+    e.preventDefault();
+    auth.signInWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+    .then( user => console.log(user) )
+    .catch( err => console.error(err) );
+  }
   return (
   <div className="signin">
     <form action="">
       <h1>Sign in</h1>
       <input type="email" ref={ emailRef } autocomplete="username" />
       <input type="password" ref={ passwordRef } autocomplete="current-password" />
-      <button>Sign in</button>
+      <button onClick={ singIn } >Sign in</button>
       <h6>Not yet register? <span className="signin-link" onClick={ singUp } >Sign up</span></h6>
     </form>
   </div>);
