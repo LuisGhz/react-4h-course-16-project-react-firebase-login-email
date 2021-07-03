0ed922606e48ec8834ea204bc0845bc8bbf88950
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-03 13:31:10 -0500

    Get user auth if exists

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { auth } from "config/firebase";
 import Home from "routes/Home";
 import Signin from "routes/Signin";
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        const us = {
+          uid: userAuth.uid,
+          email: userAuth.email
+        }
+        setUser(us);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div style={{ textAlign: 'center', paddingTop: '1rem' }}>
       {/* <Home /> */}
