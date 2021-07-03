f43d190c46cfd0d201467b5e99d7f79c06a2b3f8
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-03 13:02:40 -0500

    Create object from useRef Create objects from useRef for email and password inputs

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,12 +1,16 @@
+import useRef from 'react';
 import './Signin.css';
 
 const Signin = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   return (
   <div className="signin">
     <form action="">
       <h1>Sign in</h1>
-      <input type="text" name="" id="" />
-      <input type="password" name="" id="" />
+      <input type="email" ref={ emailRef } />
+      <input type="password" ref={ passwordRef } />
       <button>Sign in</button>
       <h6>Not yet register? <span className="signin-link" >Sign up</span></h6>
     </form>
